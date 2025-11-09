142adc1f8d82467d1b3cc6a64d2b58b978ef232e
Sachchiyay-Exports <sachchiyayexports@gmail.com>
2025-11-10 03:19:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Assuming your styles are here
+import './style.css'; // Assuming your styles are here
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
